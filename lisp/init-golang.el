9e991af0c5dea69957341578df2cb3e778bba5be
liaotonglang <liaotonglang@gmail.com>
2020-03-09 20:19:08 +0800

    auto format

--- lisp/init-golang.el
+++ lisp/init-golang.el
@@ -5,8 +5,8 @@
 ;; Set up before-save hooks to format buffer and add/delete imports.
 ;; Make sure you don't have other gofmt/goimports hooks enabled.
 (defun lsp-go-install-save-hooks ()
-  (add-hook 'before-save-hook #'lsp-format-buffer t t)
-  (add-hook 'before-save-hook #'lsp-organize-imports t t))
+  (add-hook 'before-save-hook #'lsp-format-buffer))  
+  (add-hook 'before-save-hook #'lsp-organize-imports)
 (add-hook 'go-mode-hook #'lsp-go-install-save-hooks)
 
 ;; go tags
